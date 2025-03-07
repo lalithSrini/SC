447466a714828a4197846212e46f8433877a8b1e
lalithSrini <lalithsrinivas.t@gmail.com>
2025-03-07 07:37:08 +0530

    add

--- 3_Message Authentication Code .py
+++ 3_Message Authentication Code .py
@@ -6,12 +6,12 @@ def generate_hashes(input_string):
     sha1_hash = hashlib.sha1(input_string.encode()).hexdigest()
 
     print(f"Input: {input_string}")
-    print("The hexadecimal equivalent of SHA256 is :")
-    print(sha256_hash)
-    print("The hexadecimal equivalent of SHA512 is :")
-    print(sha512_hash)
-    print("The hexadecimal equivalent of SHA1 is :")
-    print(sha1_hash)
+    print("The hexadecimal equivalent of SHA256 is :",sha256_hash)
+    # print(sha256_hash)
+    print("The hexadecimal equivalent of SHA512 is :",sha512_hash)
+    # print(sha512_hash)
+    print("The hexadecimal equivalent of SHA1 is :",sha1_hash)
+    # print(sha1_hash)
 
 str1 = "GeekforGeeks"
 generate_hashes(str1)
