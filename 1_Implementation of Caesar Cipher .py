724546aeafcf7a8afebc80b4aacabb42689457d7
lalithSrini <lalithsrinivas.t@gmail.com>
2025-03-07 07:30:29 +0530

    add

--- 1_Implementation of Caesar Cipher .py
+++ 1_Implementation of Caesar Cipher .py
@@ -4,13 +4,14 @@ def caesar_cipher(text, shift):
         if char.isalpha():  # Check if character is a letter
             shift_base = ord('A') if char.isupper() else ord('a')
             ans += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+
         else:
             ans += char  # Append non-alphabetic characters unchanged
     return ans
 
 # Define input values
-plaintext = "HELLO WORLD"
-n = 1
+plaintext = "Toga Party"
+n = 3
 
 # Encrypt the plaintext
 encrypted_text = caesar_cipher(plaintext, n)
